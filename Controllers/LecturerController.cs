f2e65355b97e49e06728db6eae8b717656378f21
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-09-10 12:39:12 +0200

    add lecturer

--- Controllers/LecturerController.cs
+++ Controllers/LecturerController.cs
@@ -1,6 +1,33 @@
-﻿namespace PROG6212_POE.Controllers;
+﻿using PROG6212_POE.Models;
 
-public class LecturerController
+namespace PROG6212_POE.Controllers;
+
+
+// Controllers/LecturerController.cs
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class LecturerController : Controller
 {
-    
+    public static List<Claim> claims = new List<Claim>();
+
+    public IActionResult SubmitClaim()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SubmitClaim(Claim claim)
+    {
+        // For now, we'll set the claim status to "Pending"
+        claim.Status = "Pending";
+        claims.Add(claim);
+        return RedirectToAction("ViewClaims");
+    }
+
+    public IActionResult ViewClaims()
+    {
+        // View submitted claims
+        return View(claims);
+    }
 }
--- Models/Lecturer.cs
+++ Models/Lecturer.cs
@@ -1,6 +1,9 @@
 ﻿namespace PROG6212_POE.Models;
 
+// Models/Lecturer.cs
 public class Lecturer
 {
-    
+    public int LecturerID { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; }
 }
