894e159ad3b70e0e02f89a651d67dc9c9236fb2b
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-11-22 19:07:49 +0200

    add HR Model

---
+++ Models/HumanResources.cs
@@ -0,0 +1,12 @@
+﻿namespace PROG6212_POE.Models;
+
+public class HumanResources
+{
+    public List<Claim> Claims { get; set; }
+    public List<Lecturer> Lecturers { get; set; }
+    public List<ProgrammeCoordinator> ProgrammeCoordinators { get; set; }
+    
+    
+    
+    
+}
