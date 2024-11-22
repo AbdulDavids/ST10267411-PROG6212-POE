413c7d7cef6c5a0f557770a0cc73837503b3399e
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-11-22 19:08:07 +0200

    add HR controller

---
+++ Controllers/HumanResourcesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROG6212_POE.Models;
+using PROG6212_POE.Controllers;
+
+namespace PROG6212_POE.Controllers
+{
+    public class HumanResourcesController : Controller
+    {
+        public static HumanResources HR = new HumanResources();
+
+        public IActionResult Dashboard()
+        {
+            var claims = LecturerController.claims; // Get claims from LecturerController
+            return View(claims);
+        }
+    }
+}
