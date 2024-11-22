fa3bc0f9f6ce4b0a4fec130bc8b5aae3bcaaa95e
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-11-22 22:36:17 +0200

    assemblyinfo

--- obj/Debug/net8.0/PROG6212-POE.AssemblyInfo.cs
+++ obj/Debug/net8.0/PROG6212-POE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6212-POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436e2a27e379a2df76230c33993297b43d62ce1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413c7d7cef6c5a0f557770a0cc73837503b3399e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6212-POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6212-POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
